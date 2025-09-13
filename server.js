7c20a0b7b40d4fda6be520116c44e88aa939a282
Vignesh Angamuhtu <vipvignesh.dr.2244@gmail.com>
2025-09-13 18:39:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const connectDB = require("./database/connection");
 const Review = require("./models/review");
 require("dotenv").config();
 const reviewRoute = require("./routes/reviewRoute")
+const contactRoute = require("./routes/contactRoute")
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -17,5 +18,7 @@ app.use(express.json());
 
 
 app.use('/api/review',reviewRoute);
+app.use("/api/contact", contactRoute);
 // Start server
 app.listen(PORT, () => console.log(`Server running on port ${PORT} ✅`));
+
